b5ddefea39f474dead23ddecac12417a8e135bfc
Valentín Peñalver <valenpga@gmail.com>
2024-04-16 21:34:00 +0200

    Ejercicio terminado

--- src/components/BoxColor.jsx
+++ src/components/BoxColor.jsx
@@ -1,14 +1,36 @@
-import React from "react";
 
-const BoxColor = ({color, userInput}) => {
-  const imagePath = `./assets/color-${color.toLowerCase()}.png`;
-  
-  return(
-    <div className={`box ${color}`}>
-      <p>{userInput}</p>
-      <p>{color === userInput ? 'Soy el color' : 'No soy el color'}</p>
+import {useRef, useState, useEffect} from 'react'
+
+export function BoxColor ({color, value}) {
+  const [classList, setClassList] = useState('')
+  const boxRef = useRef(null)
+
+  const boxCss = {
+    backgroundColor: classList.value == `box ${value}` ? color : null,
+    border: `1px solid ${color}`,
+    color: color
+
+  }
+
+  useEffect(() => {
+    setClassList(boxRef.current.classList)
+  }, [])
+
+  return (
+  <>
+    <div
+      ref={boxRef} 
+      className={`box ${color}`}
+      style={boxCss}
+    >
+    {color === value ? <h2 style={{color: 'black'}}>Yes, I´m {value} color</h2> : <h2>I´m not {value} color</h2> }
     </div>
+    </> 
   )
 }
 
-export default BoxColor;
+
+
+
+
+
--- src/components/MyFormChallenge.jsx
+++ src/components/MyFormChallenge.jsx
@@ -1,41 +1,31 @@
-import {useEffect, useState, useRef} from 'react';
-import BoxColor from './BoxColor';
 
-const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive'];
+import {useRef, useState} from 'react'
+import {BoxColor} from './BoxColor.jsx'
 
-function MyFormChallenge() {
-  const [userColor, setUserColor] = useState('');
-  const inputRef = useRef('null');
-
-  const handleInputChange = (event) => {
-    setUserColor(event.target.value);
-    onColorChange(event.target.value);
-  }
-
-  useEffect(() => {
-    inputRef.current.focus()
-    //setUserColor(inputRef.current.userColor);
-
-  }, [] )
+function MyForm() {
+  const inputRef = useRef('')
+  const [value, setValue] = useState('')
+  const colors = ['red', 'green', 'pink', 'yellow', 'purple', 'white', 'blue', 'aqua', 'olive'];
 
   return (
     <>
-    <input
-      type="text"
+    <input 
+      type='text'
+      placeholder='choose a color'
+      value={value}
       ref={inputRef}
-      placeholder="añade el color"
-      onChange={handleInputChange}
+      onChange={() => setValue(inputRef.current.value)} 
     />
-    <div style={{display: 'flex'}}>
-      {colors.map((color, index) => (
-        <BoxColor key={index} color={color} userInput={userColor}/>
-      ))}
+    <div className="container">
+    {colors.map((color, index) => {
+      return(<BoxColor key={index} color={color} value={value} />)
+    })
+    }
     </div>
+
+  
     </>
   );
-  function onColorChange(newColor) {
-    console.log('Color cambiado a :', newColor )
-  }
 }
 
-export default MyFormChallenge;
+export default MyForm;
